ad7735114aa0888b17426f2a6b60d0a5950ae232
PHOENUX\Brandon <bdstevenson4225@aol.com>
2019-05-07 19:34:15 -0400

    Base functionality done, bug fixing and code cleaning

--- liri.js
+++ liri.js
@@ -1,30 +1,40 @@
 var dotenv = require("dotenv").config();
+var moment = require('moment');
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 var fs = require("fs");
-
 var command = process.argv[2];
 var searchItem = "";
+var searchUri = "";
 var nodeArgs = process.argv;
 
+for (var i = 3; i < nodeArgs.length; i++) {
 
+    if (i > 3 && i < nodeArgs.length) {
+     searchItem = searchItem + "+" + nodeArgs[i];
+    }
+    else {
+      searchItem += nodeArgs[i];
+  
+    }
+  }
 if (command === "movie-this") {
-    getMoveie();
+    getMoveie(searchItem);
 } else if (command === "spotify-this-song") {
-    
-    getSong();
+    getSong(searchItem);
 } else if (command === "do-what-it-says") {
-    doThis();
+    doThis(searchItem);
 } else if (command === "concert-this") {
-    getConcert();
+    getConcert(searchItem);
+    console.log("search " + searchItem);
 } else {
     console.log("I have no clue what you just said...");
 }
 
-// TRILOGY
-// codingbootcamp
+
+
 
 
 function doThis() {
@@ -35,28 +45,20 @@ function doThis() {
           return console.log(error);
         }
         console.log(data);
-    })
+    });
+    logCommand("did the thing");
 }
 
-function getMoveie() {
-    searchItem = "";
-    nodeArgs = process.argv;
-    for (var i = 3; i < nodeArgs.length; i++) {
+function getMoveie(movie) {
 
-        if (i > 3 && i < nodeArgs.length) {
-         searchItem = searchItem + "+" + nodeArgs[i];
-        }
-        else {
-          searchItem += nodeArgs[i];
-      
-        }
-      }
+    nodeArgs = process.argv;
+
       if (searchItem === "") {
         searchItem = "Mr. Nobody";
     } 
-    axios.get("http://www.omdbapi.com/?t=" + searchItem + "&y=&plot=short&apikey=trilogy").then(
-        function (response) {
-            if (response.data.Response === "True") {
+    axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy")
+    .then(function (response) {
+        if (response.data.Response === "True") {
             console.log("Title: " + response.data.Title);
             console.log("Release Year: " + response.data.Year);
             console.log("imbd Rating: " + response.data.imdbRating);
@@ -65,37 +67,49 @@ function getMoveie() {
             console.log("Language: " + response.data.Language);
             console.log("Synopsis: " + response.data.Plot);
             console.log("Starring Actors: " + response.data.Actors);
-            } else if (response.data.Response === "False"){
-                console.log("You sure you typed that right?");
-            }
-        })
-        logCommand("movie this", searchItem);
+        } else if (response.data.Response === "False"){
+            console.log("You sure you typed that right?");
+        }
+    })
+    logCommand("movie this", movie);
 }
 
+function getSong(song) {
 
-
-
-
-function getSong() {
-
-
-    var searchItem = "I Want it That Way"
-    spotify.search({ type: 'track', query: searchItem, limit: 1}, function(err, data) {
+    spotify.search({ type: 'track', query: song, limit: 1}, function(err, data) {
         if (err) {
           return console.log('Error occurred: ' + err);
         }
-       
-      console.log(data.tracks[17]); 
-      });
+        console.log(data.tracks.items[0].artists[0].name);
+        console.log(data.tracks.items[0].album.name);
+        console.log(data.tracks.items[0].preview_url); 
+        console.log(data.tracks.items[0].name); 
+         searchUri = data.tracks.items[0].uri;
+    });
+    logCommand("Spotify This Song", song);
 }
 
-// function getConcert() {
-
-    // }
+function getConcert(artist) {
+// artist = "pegboard nerds"
+var getURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+console.log(getURL);
+    axios
+    .get(getURL)
+    .then(function(events) {
+            var inTown = events.data[0];
+            var date = inTown.datetime;
+            var momentDate = moment(inTown.datetime);
+        // return array of events
+        console.log("Their Next Venue is: " + inTown.venue.name);
+        console.log ("In: " + inTown.venue.city + " " + inTown.venue.region)
+        console.log("At: " + momentDate.format("MM/DD/YYYY hh:mm"));
+      })
+      logCommand("concer-this", artist);
+    }
     
-function logCommand(action, y) {
+function logCommand(action, query) {
     var lineBreak = "\n ";
-    var loggableAction = lineBreak + action + " " + y;
+    var loggableAction = lineBreak + action + " " + query;
     fs.appendFile("log.txt", loggableAction, function(err) {
 
     // If the code experiences any errors it will log the error to the console.
@@ -103,7 +117,7 @@ function logCommand(action, y) {
       return console.log(err);
     }
     // Otherwise, it will print: "movies.txt was updated!"
-    console.log(loggableAction);
+    // console.log(loggableAction);
   
   });
 }
